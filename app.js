a9c1c08b37622160e88f5046374ed5076a6b5f46
fjcoherculano <franciscoherculano.dev@incibrasil.com.br>
2022-03-08 11:03:26 -0300

    Atualização dia 08/03

---
+++ app.js
@@ -0,0 +1,52 @@
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
+
+const PORT = 3000;
+
+// Starta meu servidor na porta 3000
+app.listen(PORT, function(){
+    console.log(`O express está rodando na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extend: false}));
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Estilos da aplicação || Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+        
+    }).catch(err => {
+        console.log("Ocorreu um erro ao conectar", err);
+    });
+
+// routes
+app.get('/', (req, res) => {
+
+    Job.findAll({order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+        res.render('index', {
+            jobs
+        });
+    });
+});
+
+// Jobs routes 
+app.use('/jobs', require('./routes/jobs'));
+   
